23dcd84d5928a14e748b1d8d999844c3b8349266
Txinto Vaz <txinto@iac.es>
2017-06-15 18:44:36 +0100

    Now the system does not need the physical presence of df_folder.ods when executing subtrees.  It can execute them from the templates. It does not copy them, so a lot of files are not created in the project folders. But if the file is there, it uses the physical copy, allowing the user to change the behaviour of a single subtree.

--- examples/myproject_evolved/myproject/df_run.py
+++ examples/myproject_evolved/myproject/df_run.py
@@ -25,8 +25,12 @@ const_list_endcolumn = 2
 const_list_tab = "List"
 const_readme_file_name = "README.txt"
 
-def create_folder(project_path):
-    prj_folder_file_path = project_path + "/" + const_df_folder_name
+def create_folder(project_path, df_folder_path):
+    last_df_folder_path = ""
+    if len(df_folder_path)>0:
+        prj_folder_file_path = df_folder_path
+    else:
+        prj_folder_file_path = project_path + "/" + const_df_folder_name
 
     data = get_data(prj_folder_file_path, start_column=(const_base_column_number + const_path_column_number), column_limit=(const_template_column_number - const_path_column_number+1),
                     start_row=const_startrow, row_limit=(const_endrow - const_startrow))
@@ -38,7 +42,7 @@ def create_folder(project_path):
     cur_step = 0
     end_step = len(datares)
 
-    while (cur_step < end_step):
+    while cur_step < end_step:
         datarow = datares[cur_step]
 
         if len(datarow) > const_path_column_number:
@@ -53,12 +57,16 @@ def create_folder(project_path):
                 template_path = datarow[const_template_column_number]
                 print ("* Template is : " + template_path)
                 print ("* File to be created is : " + file_path)
-                if not os.path.exists(file_path):
-                    copyfile(template_path, file_path)
-
                 # Now we treat the case where there is a pair of "list.ods" and "def_carpeta.ods" files, in order to create
                 # recursively the subfolders
                 base_name = os.path.basename(file_path)
+                if not os.path.exists(file_path):
+                    # in the case we specify a df_folder, we do not copy it, we just remember the path of the template
+                    if base_name != const_df_folder_name:
+                        copyfile(template_path, file_path)
+                    else:
+                        last_df_folder_path = template_path
+
                 print("const_list_file_name: #" + const_list_file_name + "#")
                 print("Basename of current file_path: #" + base_name + "#")
                 if base_name == const_list_file_name:
@@ -66,8 +74,14 @@ def create_folder(project_path):
                     directory = os.path.dirname(file_path)
                     template_path = directory + "/" + const_folder_def_file_name
                     print ("* Template path for recursive folder must be in the path: " + template_path)
-                    if os.path.exists(template_path):
-			print("***** Template for recursive folder found")
+                    if not(os.path.exists(template_path)):
+                        template_path = last_df_folder_path
+                        df_folder_template_exists = os.path.exists(last_df_folder_path)
+                    else:
+                        df_folder_template_exists = True
+
+                    if df_folder_template_exists:
+                        print("***** Template for recursive folder found")
                         sub_data = get_data(file_path, start_column=const_list_startcolumn,
                                         column_limit=(const_list_endcolumn - const_list_startcolumn + 1),
                                         start_row=const_list_startrow, row_limit=(const_list_endrow - const_list_startrow + 1))
@@ -98,11 +112,11 @@ def create_folder(project_path):
                                     file.write(sub_data_list[cur_step_inner][const_inner_desc_column_number])
                                     file.write("\r\n")
                                     file.close()
-                                item_path = inner_file_path + "/" + const_folder_def_file_name
-                                if not os.path.exists(item_path):
-                                    copyfile(template_path, item_path)
+                                # item_path = inner_file_path + "/" + const_folder_def_file_name
+                                # if not os.path.exists(item_path):
+                                #     copyfile(template_path, item_path)
 
-                                create_folder(inner_file_path)
+                                create_folder(inner_file_path, template_path)
 
                             cur_step_inner += 1
                 else:
@@ -124,4 +138,4 @@ def create_folder(project_path):
 
 
 
-create_folder(".")
+create_folder(".",const_df_folder_name)
