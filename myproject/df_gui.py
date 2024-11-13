9a311ed78bc565c9b70816f12d8a03fb246e73b8
Txinto Vaz <txinto@elporis.com>
2024-11-13 17:43:51 +0000

    Updating df_gui.py to new structure and Python3

--- myproject/df_gui.py
+++ myproject/df_gui.py
@@ -7,5 +7,6 @@ ret = df_run_msg
 while ret == df_run_msg:
     ret = g.buttonbox(msg='Click to Generate or update the folder structure', title='Deepfolders', choices=(['Generate']))
     if ret == df_run_msg:
-        execfile("df_run.py")
+        filename = ".scripts/df_run.py"
+        exec(compile(open(filename, "rb").read(), filename, 'exec'))        
 
